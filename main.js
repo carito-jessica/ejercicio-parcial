632e49728ce1ce1f0b2d50a8b5205a7bc2afd904
carito-jessica <cjcacciagiu16@gmail.com>
2023-10-01 21:18:14 -0300

    Tarjeta para cada pokemon
    Donde se muestre el nombre, url , la img y cuando toquen el ver más despues se vea su otra info con una modal

--- main.js
+++ main.js
@@ -18,3 +18,38 @@ const getPokemon = async (id) => {
     const apiUrl = `https://pokeapi.co/api/v2/pokemon/${id}`;
     return { data, apiUrl };
 }
+
+
+// Mostrar los 100 Pokemon
+const cantidadPokemon = async () => {
+    for (let id = 1; id <= 100; id++) {
+        const pokemon = await getPokemon(id);
+        if (!pokemon) continue;
+        
+        const card = carta(pokemon.data);
+        contenido.appendChild(card);
+    }
+};
+
+// Función para crear una tarjeta de Pokémon
+const carta = (pokemon) => {
+    const card = document.createElement('div');
+    card.classList.add('pokemon-card');
+    card.innerHTML = `
+        <h3>${pokemon.name}</h3>
+        <p> <span class="api-url"></span></p> 
+        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+        <button class="view-details" data-pokemon-id="${pokemon.id}">Ver Más</button>
+    `;
+    // url 
+    const url = card.querySelector('.api-url');
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon.id}`;
+    url.textContent = apiUrl;
+
+    // Agregado: Event listener para mostrar detalles
+    card.querySelector('.view-details').addEventListener('click', () => {
+        detalles(pokemon.id);
+    });
+
+    return card;
+};
